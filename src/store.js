008f543c3129ff452923f56b29654396bb868d2a
sj9608 <sneak7581@gmail.com>
2021-08-09 18:22:53 +0900

    [ADD] vuex로 구성한 todo (v0.1)

--- src/store.js
+++ src/store.js
@@ -4,19 +4,24 @@ export const store = createStore({
     state() {
         
         return {
-        
+            todos: [],
         }
     },
 
     mutations: {
-
+        SET_TODO(state, payload) {
+            state.todos = [...state.todos, payload]
+            localStorage.setItem("todo", JSON.stringify(state.todos))
+        }
     },
 
     actions: {
-
+        addTodo({commit}, payload) {
+            commit('SET_TODO', payload)
+        }
     },
 
     getters: {
-
+        todos: state => state.todos,
     }
 });
---
+++ src/views/todo.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <h1>Today's Todo</h1>
+    <input
+      type="text"
+      v-model="newTodo"
+      placeholder="What needs to be done?"
+      @keyup.enter="addTodo"
+      autofocus
+    />
+    <ul>
+      <li v-for="todo in todos" :key="todo">
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  computed: {
+    todos() {
+      return this.$store.getters["todos"];
+    },
+  },
+
+  methods: {
+    addTodo() {
+      this.$store.dispatch("addTodo", this.newTodo);
+      this.newTodo = "";
+    },
+  },
+
+  mounted() {
+    
+  },
+};
+</script>
+<style>
+</style>
