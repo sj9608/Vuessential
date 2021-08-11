f2c7d51902d14ed61a151ded9608d807b5f7e205
sj9608 <sneak7581@gmail.com>
2021-08-11 10:54:01 +0900

    [ADD] styles import

--- vue.config.js
+++ vue.config.js
@@ -6,6 +6,10 @@ module.exports = {
                 additionalData: `
                     @import "@/assets/styles/_variables.scss";
                     @import "@/assets/styles/_mixins.scss";
+                    @import "@/assets/styles/_svg.scss";
+                    @import "@/assets/styles/_common.scss";
+                    @import "@/assets/styles/_mediaQueries.scss";
+                    @import "@/assets/styles/_reset.scss";
                     `
             }
         }
