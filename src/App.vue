ec2a64d6dacca51a3e2af3bd6b2cd1897ac59a76
sj9608 <sneak7581@gmail.com>
2021-08-11 17:48:28 +0900

    [ADD] App 스타일 추가

--- src/App.vue
+++ src/App.vue
@@ -16,6 +16,16 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss">
+.top {
+  width: 100%;
+  min-height: 43.6rem;
+  padding: 0 $padding 4.5rem;
+  background-image: linear-gradient(145deg, #3770cc 20%, #ce91c9 80%);
+}
 
+.body {
+  padding: 3rem $padding;
+  background-color: #efefef;
+}
 </style>
