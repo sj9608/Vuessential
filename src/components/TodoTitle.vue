fb9fe683b8e8b1413b2e7cfa5a0d1c7e4f3ad71e
sj9608 <sneak7581@gmail.com>
2021-08-11 10:53:17 +0900

    [ADD] Title 스타일 추가

--- src/components/TodoTitle.vue
+++ src/components/TodoTitle.vue
@@ -1,9 +1,10 @@
 <template>
     <div class="title">
-        <p class="title_mesage">{{ message }}</p>
-        <p class="title_task">
-            You've got
-            <span class="title_task_total">{{ taskTotal }}</span> tasks today.
+        <p class="title__mesage">{{ message }}</p>
+        <p class="title__task">
+            <span class="title__task-top">You've got </span>
+            <span class="title__task-total">{{ taskTotal }}</span>
+            <span class="title__task-bottom"> tasks today.</span>
         </p>
     </div>
 </template>
@@ -17,6 +18,37 @@ export default {
     }
 }
 </script>
-<style>
-    
+<style lang="scss">
+.title {
+    max-width: $max-width;
+    margin: 0 auto;
+    letter-spacing: 0.03rem;
+    color: #fff;
+
+    &__mesage {
+        font-size: 1.6rem;
+    }
+
+    &__task {
+        margin-top: 3.5rem;
+        margin-bottom: 3rem;
+        font-weight: bold;
+
+        &-top {
+            display: block;
+            font-size: 1.8rem;
+        }
+
+        &-total {
+            font-size: 4.4rem;
+            line-height: 110%;
+        }
+
+        &-bottom {
+            display: block;
+            font-size: 1.8rem;
+            line-height: 100%;
+        }
+    }
+}
 </style>
